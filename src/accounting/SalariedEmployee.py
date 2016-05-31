831a84f622238fe0dd3c898be0af8bf6092371a9
paul <konrad46@hotmail.com>
2016-05-31 01:01:22 -0400

    edit

--- src/accounting/SalariedEmployee.py
+++ src/accounting/SalariedEmployee.py
@@ -1,18 +1,30 @@
 class SalariedEmployee:
 
-    def __init__(self, employee, first, last):
-            self.__employee_id = employee
-            self.__first_name = first
-            self.__last_name = last
+    def __init__(self, employee_id, first_name, last_name, salary, commission_rate, weekly_dues):
+        self.__employee_id = employee_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__salary = salary
+        self.__commission_rate = commission_rate
+        self.__weekly_dues = weekly_dues
 
-    def set_employee(self, employee):
-            self.__employee_id = employee
+    def set_employee_id(self, employee_id):
+        self.__employee_id = employee_id
 
-    def set_first(self, first):
-            self.__first_name = first
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
 
-    def set_last(self, last):
-            self.__last_name = last
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
 
-    def get_full_name(self, last, first):
-            print("Employee: " + last + ", " + first)
+    def set_salary(self, salary):
+        self.__salary = salary
+
+    def set_salary(self, commission_rate):
+        self.__commission_rate = commission_rate
+
+    def set_weekly_dues(self, weekly_dues):
+        self.__weekly_dues = weekly_dues
+
+    def get_full_name(self):
+        return self.__last_name + ', ' + self.__first_name
